d50f68f137e8fc066b15281ed05cd426b7b3dc4e
gwierzchowski <gwierzchowski@wp.pl>
2021-03-20 21:28:42 +0100

    Test that deserializers are Send

--- src/lib.rs
+++ src/lib.rs
@@ -346,32 +346,40 @@ mod tests {
             }
           
             #[test]
-            fn test_on_files_serde() {
+            fn test_on_files_serde_send() {
                 use std::io::Read;
                 use std::hash::Hasher;
                 std::fs::create_dir_all("examples/data").unwrap();
                 let file_in  = "examples/data/smallpop_serde.csv";
                 let file_out = "examples/data/smallpop_serde_out.csv";
 
+                // Below code requires / check that deserializers are Send.
                 #[cfg(not(feature = "tokio"))]
-                async_std::task::block_on(async {
-                    if let Err(err) = create_async(file_in).await {
-                        assert!(false, "error running create_async: {}", err);
-                    }
-                    if let Err(err) = copy_async_serde(file_in, file_out).await {
-                        assert!(false, "error running copy_async_serde: {}", err);
-                    }
-                });
+                {
+                    let jh = async_std::task::spawn(async move {
+                        if let Err(err) = create_async(file_in).await {
+                            assert!(false, "error running create_async: {}", err);
+                        }
+                        if let Err(err) = copy_async_serde(file_in, file_out).await {
+                            assert!(false, "error running copy_async_serde: {}", err);
+                        }
+                    });
+                    async_std::task::block_on(jh);
+                }
                 #[cfg(feature = "tokio")]
-                tokio::runtime::Runtime::new().unwrap().block_on(async {
-                    if let Err(err) = create_async(file_in).await {
-                        assert!(false, "error running create_async: {}", err);
-                    }
-                    if let Err(err) = copy_async_serde(file_in, file_out).await {
-                        assert!(false, "error running copy_async_serde: {}", err);
-                    }
-                });
-                
+                {
+                    let rt = tokio::runtime::Runtime::new().unwrap();
+                    let jh = rt.spawn(async move {
+                        if let Err(err) = create_async(file_in).await {
+                            assert!(false, "error running create_async: {}", err);
+                        }
+                        if let Err(err) = copy_async_serde(file_in, file_out).await {
+                            assert!(false, "error running copy_async_serde: {}", err);
+                        }
+                    });
+                    rt.block_on(jh).unwrap();
+                }
+
                 let mut bytes_in  = vec![];
                 std::fs::File::open(file_in).unwrap().read_to_end(&mut bytes_in).unwrap();
                 let mut hasher_in = std::collections::hash_map::DefaultHasher::new();
